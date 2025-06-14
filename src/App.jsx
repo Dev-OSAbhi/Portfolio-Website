4137a8a98c86f64d1551903826bf18d2a3691297
Abhinav Kumar <abhinav03ks@gmail.com>
2025-06-14 06:43:10 +0000

    feat: initial setup and nav bar setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
-export default function App() {
-  return (
-    <h1>Goddess of Discord</h1>
-  );
-}
+import NavBar from "./components/NavBar";
 
+const App = () => {
+  return <main className="max-w-7xl mx-auto">
+    <NavBar />
+  </main>
+};
+
+export default App;
