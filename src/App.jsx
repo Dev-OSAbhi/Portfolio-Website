b1f99f6c47be95d38db56f112150bb109a56a26b
Abhinav Kumar <abhinav03ks@gmail.com>
2025-06-14 10:27:00 +0000

    feat : added the 3d elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import Hero from "./components/Hero";
 import NavBar from "./components/NavBar";
 
 const App = () => {
   return <main className="max-w-7xl mx-auto">
     <NavBar />
+    <Hero />
   </main>
 };
 
---
+++ src/components/Hero.jsx
@@ -0,0 +1,56 @@
+import { PerspectiveCamera } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+import { Suspense } from "react";
+import { useMediaQuery } from "react-responsive";
+import { calculateSizes } from "../constants/index.js";
+import CanvasLoader from "./CanvasLoader.jsx";
+import Cube from "./Cube.jsx";
+import HackerRoom from "./HackerRoom.jsx";
+import ReactLogo from "./ReactLogo.jsx";
+import Rings from "./Ring.jsx";
+import Target from "./Target.jsx";
+
+const Hero = () => {
+
+  const isSmall = useMediaQuery({ maxWidth: 440 });
+  const isMobile = useMediaQuery({ maxWidth: 768 });
+  const isTablet = useMediaQuery({ minWidth: 768, maxWidth: 1024 });
+
+  const sizes = calculateSizes(isSmall, isMobile, isTablet);
+
+  return (
+    <section className="min-h-screen w-full flex flex-col relative">
+      <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
+        <p className="sm:text-3xl text-2xl font-medium text-white text-center font-generalsane">
+          Hi, I am Abhinav <span className="waving-hand">👋</span>
+        </p>
+        <p className="hero_tag text-gray_gradient">
+          Building Products and Brands
+        </p>
+      </div>
+      <div className="w-full h-full absolute inset-0">
+        <Canvas className="w-full h-full" >
+          <Suspense fallback={<CanvasLoader />}>
+            <PerspectiveCamera makeDefault position={[0, 0, 30]} />
+            <HackerRoom
+              position={sizes.deskPosition}
+              rotation={[0, -Math.PI, 0]}
+              scale={sizes.deskScale}
+            />
+            <group>
+              <Target position={sizes.targetPosition} />
+              <ReactLogo position={sizes.reactLogoPosition} />
+              <Cube position={sizes.cubePosition} />
+              <Rings position={sizes.ringPosition} />
+            </group>
+            <ambientLight intensity={1} />
+            <directionalLight intensity={0.5} position={[10, 10, 10]} />
+          </Suspense>
+        </Canvas>
+      </div>
+      Hero
+    </section >
+  )
+};
+
+export default Hero;
